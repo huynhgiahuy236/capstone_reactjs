417b57a243c885c49f6ce45173081cb54b4c8963
huynhgiahuy <huynhgiahuy236@gmail.com>
2026-07-08 16:57:54 +0700

    feat: add delete functionality for showtimes and enhance user listing with search capability

--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Header from '../components/Header'
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/Footer'
+import { AdminFeedbackProvider } from '../components/AdminFeedback'
 
 const HomeLayout = () => {
   return (
-    <div>
+    <AdminFeedbackProvider>
       <Header />
 
       <Outlet />
 
       <Footer />
-    </div>
+    </AdminFeedbackProvider>
   )
 }
 
